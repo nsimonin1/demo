6a7823a547768eeb24f09f7269d05d2ef08dc341
SimonPascal <nsimonin1@yahoo.fr>
2016-06-07 20:40:58 +0400

    init project

---
+++ src/main/java/org/simon/pascal/DemoApplication.java
@@ -0,0 +1,12 @@
+package org.simon.pascal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
